5ec41c86ee3decee8b7c4f82caba6d2d364b3bde
Felipe Sousa <47900701+FelipeSdsilva@users.noreply.github.com>
2023-04-28 19:38:16 -0300

    update metodoMenu (#8)
    Co-authored-by: Ana <anarosa.af@outlook.com>

--- src/entities/Hunter.java
+++
@@ -1,7 +0,0 @@
-package entities;
-
-public class Hunter extends Heroi{
-    public Hunter(String nome, int hp, int stamina, int mana, float altura, String gender, String raca) {
-        super(nome, hp, stamina, mana, altura, gender, raca);
-    }
-}
