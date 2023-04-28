86bf7205385d785b15e24c80c34e5cfb40d46242
Felipe Sousa <47900701+FelipeSdsilva@users.noreply.github.com>
2023-04-28 17:24:36 -0300

    Felipe (#7)
    * Inicialização do projeto
    
    * Implementação dos métodos loreInicial, inicialMenu, escolhaDeClasse, limpaTela e impressaoLentaPorCaracter
    
    * Criação do package controller e classe controleMenu
    
    * Criação do package controller e classe controleMenu
    
    * Create a classe abstrata Heroi e as classes Arcano, Hunter, Kinght
    
    * Inclusão de excpitons
    
    * Inicialização do projeto
    
    * Incluindo arquivos do sts
    
    * Teste

--- src/controller/ControleMenu.java
+++ src/controller/ControleMenu.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.InputMismatchException;
+
 import java.util.Scanner;
 
 import static views.MenuView.*;
@@ -8,19 +10,37 @@ public class ControleMenu {
 
     public static void controleInicial() {
 
-        Scanner sc = new Scanner(System.in);
-        int op;
 
-        loreInicial();
+        try {
+            Scanner sc = new Scanner(System.in);
+            int op = 0;
 
-        inicialMenu();
-        op = sc.nextInt();
-        switch (op) {
-            case 1 -> System.out.println("");
-            case 2 -> System.out.println("");
-            case 3 -> System.out.println("");
-            default -> throw new IllegalArgumentException("Unexpected value: " + op);
+            introLore();
+
+            inicialMenu();
+
+            op = sc.nextInt();
+
+
+            switch (op) {
+                case 1 -> iniciarJogo();
+                case 2 -> iniciarJogo();
+                case 3 -> iniciarJogo();
+                default -> throw new IllegalArgumentException("Unexpected value: " + op);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Errou !! (Fausto silva)");
+            inicialMenu();
+            controleInicial();
+        } catch (IllegalArgumentException ex) {
+
         }
+    }
+
+   //Metodo para iniciar jogo.
+    public static void iniciarJogo() {
+        loreInicial();
+        escolhaDeClasse();
     }
 
 
