434d8d41bec7dbd27ab6fef7f4fdcb83963d6619
Felipe Sousa <felipe.fps09@hotmail.com>
2023-04-28 15:28:14 -0300

    Inclusão de excpitons

--- src/controller/ControleMenu.java
+++ src/controller/ControleMenu.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static views.MenuView.*;
@@ -8,20 +9,39 @@ public class ControleMenu {
 
     public static void controleInicial() {
 
-        Scanner sc = new Scanner(System.in);
-        int op;
+        try {
+            Scanner sc = new Scanner(System.in);
+            int op = 0;
 
-        loreInicial();
+            introLore();
 
-        inicialMenu();
-        op = sc.nextInt();
-        switch (op) {
-            case 1 -> System.out.println("");
-            case 2 -> System.out.println("");
-            case 3 -> System.out.println("");
-            default -> throw new IllegalArgumentException("Unexpected value: " + op);
+            inicialMenu();
+
+            op = sc.nextInt();
+
+
+            switch (op) {
+                case 1 -> iniciarJogo();
+                case 2 -> iniciarJogo();
+                case 3 -> iniciarJogo();
+                default -> throw new IllegalArgumentException("Unexpected value: " + op);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Errou !! (Fausto silva)");
+            inicialMenu();
+            controleInicial();
+        } catch (IllegalArgumentException ex) {
+
         }
     }
 
+
+    public static void iniciarJogo() {
+        loreInicial();
+
+        escolhaDeClasse();
+
+
+    }
 
 }
--- src/views/MenuView.java
+++ src/views/MenuView.java
@@ -6,13 +6,34 @@ public class MenuView {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_BLUE = "\u001B[34m";
 
-    public static void loreInicial() {
-        String inicio = """	
+
+    public static void introLore(){
+        String intro = """
                 Terra média SÉC XIV.
                                 
                 Bem vindo ao reino de Helmont um mundo mágico repleto de montanhas congeladas 
                 , planícies, pântanos obscuros e criaturas místicas, que é governado pelo 
                 Rei Calander Ultric.
+                """;
+        impressaoLentaPorCaracter(intro);
+    }
+
+    public static void loreInicial() {
+        String inicio = """	
+                
+                Ultric era um Rei que governava com punhos fortes, ambicioso e dominador,
+                casado com a rainha Nefereti Belmond e seus 3 filhos, Nagiel, Aquio e Estrid.
+                Em uma noite de comemoração de suas vitórias em campo de batalha, o rei Ultric 
+                convoca seu bobo da corte para alegrar a festa com seus contos. Em uma de suas
+                histórias a rainha se sente insultada pelas palavras proferidas vindas do bobo,
+                o rei observando aquilo e para preservar a honra de sua rainha, ordena que matem
+                o tal, e jogue seus restos no pântano de Breakwood.
+                
+                
+                A jornada se inicia em um pequeno vilarejo ao norte de Helmont, onde seu povoado
+                estava se destacando e caminhando para a independência, fugindo das mãos do rei 
+                Ultric. Com isso houve uma retaliação por parte do rei ao povo da Kocorico Village.
+                               
                 """;
         impressaoLentaPorCaracter(inicio);
     }
