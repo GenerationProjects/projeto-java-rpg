249a583faff803b5d8fe6812280298f99db34a40
Felipe Sousa <47900701+FelipeSdsilva@users.noreply.github.com>
2023-04-29 11:43:16 -0300

    Felipe (#10)
    * Inicialização do projeto
    
    * Implementação dos métodos loreInicial, inicialMenu, escolhaDeClasse, limpaTela e impressaoLentaPorCaracter
    
    * Criação do package controller e classe controleMenu
    
    * Criação do package controller e classe controleMenu
    
    * Create a classe abstrata Heroi e as classes Arcano, Hunter, Kinght
    
    * Inclusão de excpitons
    
    * Inicialização do projeto
    
    * Incluindo arquivos do sts
    
    * Teste
    
    * Incluindo package repository e classe GameRepository
    
    * Update class
    
    * Felipe (#9)
    
    * Inicialização do projeto
    
    * Implementação dos métodos loreInicial, inicialMenu, escolhaDeClasse, limpaTela e impressaoLentaPorCaracter
    
    * Criação do package controller e classe controleMenu
    
    * Criação do package controller e classe controleMenu
    
    * Create a classe abstrata Heroi e as classes Arcano, Hunter, Kinght
    
    * Inclusão de excpitons
    
    * Inicialização do projeto
    
    * Incluindo arquivos do sts
    
    * Teste

--- src/entities/Guerreiro.java
+++ src/entities/Guerreiro.java
@@ -1,8 +1,11 @@
 package entities;
 
 public class Guerreiro extends Heroi {
+    private int resistencia;
 
-    public Guerreiro(String nome, int hp, int stamina, int mana, float altura, String gender, String raca) {
-        super(nome, 1000, 500, 0, altura, gender, raca);
+    public Guerreiro(String nome, int hp, int stamina, int mana, float altura, String gender, String raca, int resistencia) {
+        super(nome, hp, stamina, mana, altura, gender, raca);
+        this.resistencia = resistencia;
     }
-}
+
+
