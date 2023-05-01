da8a3c601cf9df3ba8c60231ecdba42ca23afe8f
Ana <anarosa.af@outlook.com>
2023-05-01 18:54:11 -0300

    fix controller

--- src/entities/Guerreiro.java
+++ src/entities/Guerreiro.java
@@ -1,11 +1,10 @@
 package entities;
 
 public class Guerreiro extends Heroi {
-    private int resistencia;
 
-    public Guerreiro(String nome, int hp, int stamina, int mana, float altura, String gender, String raca, int resistencia) {
-        super(nome, hp, stamina, mana, altura, gender, raca);
-        this.resistencia = resistencia;
+    public Guerreiro(String nome, int hp, float altura, String gender, String raca) {
+        super(nome, hp, altura, gender, raca);
     }
+}
 
 
