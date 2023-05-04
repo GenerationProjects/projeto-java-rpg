0f8aa6d028c752ad615aff33ee096dad6bc7d984
Felipe Sousa <47900701+FelipeSdsilva@users.noreply.github.com>
2023-05-03 21:44:58 -0300

    Felipe (#28)
    * Resolvendo os conflitos
    
    * Implementação do métodos quartaPartedaHistoria criação da classe HistoriaView

--- src/controller/ControllerHeroi.java
+++ src/controller/ControllerHeroi.java
@@ -7,63 +7,58 @@ import java.util.ArrayList;
 
 public class ControllerHeroi implements HeroiRepository {
 
-	private ArrayList<Heroi> herois = new ArrayList<>();
-	int hpHeroi;
+    private ArrayList<Heroi> herois = new ArrayList<>();
+    int hpHeroi;
 
-	@Override
-	public void adicionarHeroi(Heroi heroi) {
-		herois.add(heroi);
-	}
+    @Override
+    public void adicionarHeroi(Heroi heroi) {
+        herois.add(heroi);
+    }
 
-	@Override
-	public String mostrarnome() {
-		String nome = "";
-		for (Heroi heroi : herois) {
-			nome = heroi.getNome();
-			hpHeroi = heroi.getHp();
-		}
-		return nome;
-	}
+    @Override
+    public String mostrarnome() {
+        String nome = "";
+        for (Heroi heroi : herois) {
+            nome = heroi.getNome();
+            hpHeroi = heroi.getHp();
+        }
+        return nome;
+    }
 
-	@Override
-	public int atacar(int dado) {
-		if (dado == 1) {
-			System.out.println("O monstro se defendeu!");
-			return 0;
-		} else if (dado > 1 && dado < 7) {
-			System.out.println("O seu ataque quase não deu dano e o monstro riu da sua cara!");
-			return 1000;
-		} else if (dado > 7 && dado < 20) {
-			System.out.println("O seu ataque deu bastante dano, mas não matou o monstro, o que o deixou enfurecido!");
-			return 3000;
-		} else if (dado == 20) {
-			System.out.println("O seu ataque foi tão forte que matou o monstro na hora!");
-			return 5000;
-		}
-		return dado;
-	}
+    @Override
+    public int atacar(int dado) {
+        if (dado == 1) {
+            System.out.println("O monstro se defendeu!");
+            return 0;
+        } else if (dado > 1 && dado < 7) {
+            System.out.println("O seu ataque quase não deu dano e o monstro riu da sua cara!");
+            return 1000;
+        } else if (dado > 7 && dado < 20) {
+            System.out.println("O seu ataque deu bastante dano, o que o deixou enfurecido!");
+            return 3000;
+        } else if (dado == 20) {
+            System.out.println("O seu ataque foi tão forte que matou o monstro na hora!");
+            return 15000;
+        }
+        return dado;
+    }
 
-	@Override
-	public int defender(int dado) {
-		if (dado == 1) {
-			System.out.println("Você tomou muito dano e morreu.");
-			return 2000;
-		} else if (dado > 1 && dado < 7) {
-			System.out.println("Você tomou bastante dano, mas não morreu!");
-			return 900;
-		} else if (dado > 7 && dado < 20) {
-			System.out.println("Você quase não tomou dano e riu da cara do monstro!");
-			return 300;
-		} else if (dado == 20) {
-			System.out.println("A sua defesa foi tão boa que você não levou dano!");
-			return 0;
-		}
-		return dado;
-	}
+    @Override
+    public int defender(int dado) {
+        if (dado == 1) {
+            System.out.println("Você tomou muito dano e morreu.");
+            return 2000;
+        } else if (dado > 1 && dado < 7) {
+            System.out.println("Você tomou bastante dano, e está sangrande!");
+            return 700;
+        } else if (dado > 7 && dado < 20) {
+            System.out.println("Você quase não tomou dano e riu da cara do monstro!");
+            return 100;
+        } else if (dado == 20) {
+            System.out.println("A sua defesa foi tão boa que você não levou dano!");
+            return 0;
+        }
+        return dado;
+    }
 
-	public void recuperarHP() {
-		for (Heroi heroi : herois) {
-			heroi.setHp(2000);
-		}
-	}
 }
